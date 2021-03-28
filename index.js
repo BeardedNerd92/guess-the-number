c3937b5d929c27dc58d76153b4f8845c3c02c00b
BeardedNerd92 <IntMain92@gmail.com>
2021-03-28 12:06:10 -0500

    Guess the number game built using vanilla javascript

---
+++ index.js
@@ -0,0 +1,42 @@
+let secretNum = 50
+let numOfTries = 0
+let btn = document.querySelector("#btn")
+
+btn.addEventListener("click", function() {
+
+    let output = document.querySelector("#output")
+    let warning = document.querySelector(".warning")
+    let input = document.querySelector(".input").value
+    let tries = document.querySelector("#tries")
+    let btnClear = document.querySelector("#btn-2");
+    btnClear.disabled = true;
+
+    function timeDelay() {
+        setTimeout(delay, 3000)
+    }
+     
+   do {
+
+    if (input == secretNum) {
+        output.innerHTML = `Correct!` 
+        numOfTries++
+        tries.innerHTML = `The total number of tries = ${numOfTries}`
+        btnClear.disabled = false;
+    }
+    else if (input == "") {
+        warning.innerHTML = "Warning: Input field must not be empty!"
+        warning.innerHTML = ""
+    }
+    else if (input > secretNum) {
+        output.innerHTML = "Try again, number inputed was to high!"
+        numOfTries++
+    }
+    else if (input < secretNum) {
+        output.innerHTML = "Try again, number inputed was to small!"
+        numOfTries++
+    }
+
+} while(false)
+});
+
+

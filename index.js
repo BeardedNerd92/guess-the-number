67b8a4259514e961a637e3ed5b7748361269b43b
BeardedNerd92 <IntMain92@gmail.com>
2021-03-30 15:31:00 -0500

    Takes users and welcomes them on the next page

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-let secretNum = 50
+let secretNum = Math.floor(Math.random() * 50)
 
 let numOfTries = 0
 
+let playerNameCard = document.querySelector("#player-name-card")
+
+let cardDisplay = document.querySelector("#card-handler")
+
+let mainClass = document.querySelector("#main_class")
+
+let welcome = document.querySelector(".welcome")
+
+
 let btn = document.querySelector("#btn")
-
 btn.addEventListener("click", function() {
-
-    let cardDisplay = document.querySelector("#card-handler")
 
     let output = document.querySelector("#output")
 
-    let input = document.querySelector(".input").value
+    let input = document.querySelector("#users-input").value
 
     let tries = document.querySelector("#tries")
 
     let btnClear = document.querySelector("#btn-2");
     btnClear.disabled = true;
 
-    let closeBtn = document.querySelector("#button")
-    closeBtn.addEventListener("click", function() {
-        cardDisplay.style.display = "none"
-    })
-
-
    do {
 
     if (input == secretNum) {
-        output.innerHTML = `Correct! ${input} is the number I chose` 
+        output.innerHTML = `Correct! ${input} is the number I am looking for.` 
         numOfTries++
         tries.innerHTML = `It took you ${numOfTries} try/tries to guess the correct number.`
         btnClear.disabled = false;
@@ -46,6 +46,22 @@ btn.addEventListener("click", function() {
 } while(false)
 });
 
+
+let closeBtn = document.querySelector("#button")
+closeBtn.addEventListener("click", function() {
+     cardDisplay.style.display = "none"
+})
+
+let playGameBtn = document.querySelector("#play-game-btn")
+playGameBtn.addEventListener("click", function() {
+    let playerName = document.querySelector("#player-name").value
+    playerNameCard.style.display = "none"
+    mainClass.style.display = "block"
+    if (playerName !== "")
+        welcome.innerHTML = `Welcome ${playerName}`.toUpperCase()
+    if (playerName == "")
+        welcome.innerHTML = "Welcome Player"
+})
 
 
 

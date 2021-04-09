dda18219d41e784dbd8a909e9092f4fc19b10a01
BeardedNerd92 <IntMain92@gmail.com>
2021-04-09 13:30:32 -0500

    Updated

--- index.js
+++ index.js
@@ -27,57 +27,55 @@ btn.addEventListener("click", function() {
     let tries = document.querySelector("#tries")
 
     let btnClear = document.querySelector("#btn-2");
-    btnClear.disabled = true;
+
+    let invalidSign = document.querySelector('#invalid-sign')
+
+    let border = document.querySelector('#border')
+
+
+    
 
    do {
 
-    if (input == secretNum) {
-        output.innerHTML = `Correct! ${input} is the number I am looking for.` 
-        numOfTries++
-        tries.innerHTML = `It took you ${numOfTries} try/tries to guess the correct number.`
-        btnClear.disabled = false;
-    }
-    if (input === "") {
-        invalidInput.innerHTML = 'Input field must not be blank.'
-        output.style.display = 'none'
-    }
-    if(input !== ''){
-        output.style.display = 'block'
-        invalidInput.style.display = 'none'
-    }
-    if (input > secretNum) {
-        output.innerHTML = `Try again, the number ${input} is too high!`
-        numOfTries++
-    }
-    if (input < secretNum) {
-        output.innerHTML = `Try again, the number ${input} is too small!`
-        numOfTries++
-    }
+        if (input == secretNum) {
+            output.innerHTML = `Correct! ${input} is the number I am looking for.` 
+            numOfTries++
+            tries.innerHTML = `It took you ${numOfTries} try/tries to guess the correct number.`
+            btnClear.style.display = 'block';
+        }
+        if (input === "") {
+            invalidInput.style.display = 'block'
+            output.style.display = 'none'
+            invalidSign.style.display = 'block'
+            border.style.borderColor = 'red'
+        }
+        if(input !== ''){
+            output.style.display = 'block'
+            invalidInput.style.display = 'none'
+            invalidSign.style.display = 'none'
+        }
+        if (input > secretNum) {
+            output.innerHTML = `Try again, the number ${input} is too high!`
+            numOfTries++
+        }
+        if (input < secretNum) {
+            output.innerHTML = `Try again, the number ${input} is too small!`
+            numOfTries++
+        }
 
-} while(false)
+    } while(false)
+
 });
 
 
-let closeBtn = document.querySelector("#button")
-closeBtn.addEventListener("click", function() {
-     cardDisplay.style.display = "none"
+
+
+
+
+let toggleOn = document.querySelector('#toggle-on')
+toggleOn.addEventListener('click', function(){
+    
+    let darkMode = document.body
+    darkMode.classList.toggle("dark-mode")
+    
 })
-
-let playGameBtn = document.querySelector("#play-game-btn")
-playGameBtn.addEventListener("click", function() {
-    let playerName = document.querySelector("#player-name").value
-    playerNameCard.style.display = "none"
-    mainClass.style.display = "block"
-
-
-    if (playerName !== "")
-        welcome.innerHTML = `Welcome ${playerName}`.toUpperCase()
-
-    if (playerName == "")
-        welcome.innerHTML = "Welcome Player"
-
-})
-
-
-
-
